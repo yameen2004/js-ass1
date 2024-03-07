da1b01e87ac04392159ee89ecd3a698e9c7933ba
yameen2004 <138603810+yameen2004@users.noreply.github.com>
2024-03-08 01:41:22 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,107 @@
+// CHAPTER 1
+
+// Q 1
+
+var name = prompt("Hello! What's your name?");
+alert("Hello, " + name + "! Welcome to our website!");
+
+// Q 2
+
+alert("Please enter a valid password")
+
+// Q 3 
+
+alert("Welcom to Js land.... \n Happy coding")
+
+// Q 4
+
+alert("Welcom to Js land...")
+
+// Q 5
+
+alert("Hello... I can run JS through my web browser's console ")
+
+
+
+
+
+// CHAPTER 2
+
+
+// Q 1
+
+var username;
+
+// Q 2
+
+var myNmae = "Muhammad Yameen"
+alert(myNmae)
+
+// Q 3
+
+var message = "Hellow World"
+alert(message)
+
+// Q 4
+
+var student = "Yameen"
+alert(student)
+
+var age = "20 years old"
+alert(age)
+
+var qualification = "Currently enrolled WEB & MOBILE APP DEVELOPMENT IN (S.M.I.T)"
+alert(qualification)
+
+// Q 5
+
+alert("Pizza \n Pizz \n Piz \n Pi \n P")
+
+// Q 6
+
+var email = "muhammadyammeen0311@gmail.com";
+var message = "My email address is: " + email;
+
+alert(message);
+
+// Q 7
+
+var book = " A smarter way to learn JavaScript "
+alert("I am trying to learn from the book " + book)
+
+// Q 8
+
+document.write("Yah! I can write HTML content through JavaScript ")
+
+// Q 9
+
+var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(design)
+
+
+
+
+// CHAPTER 3
+
+// Q 1
+
+var age = "I am 19 years old"
+alert(age)
+
+// Q 3
+
+var birthYear = "My birth year is 2004"
+document.write(birthYear)
+
+// Q 4
+
+var visitorName = "John Doe";
+var productTitle = "T-shirt";
+var quantity = 5;
+var company = "classi jeans"
+
+document.write(visitorName)
+document.write(productTitle)
+document.write(quantity)
+document.write(company)
+
